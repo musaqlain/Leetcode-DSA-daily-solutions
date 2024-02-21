323ab72707dd04adef942aa6f88090df5e0d11f6
Muhammad Saqlain <77551019+MuhammadSaqlain-dev@users.noreply.github.com>
2024-02-21 23:56:19 +0500

    Time: 0 ms (100.00%) | Memory: 7.8 MB (11.25%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count = 0;
+        int it = s.size()-1;
+        while (s[it] == ' ') {
+            it--;
+        }
+        while(it>=0 && s[it] != ' ') {
+            it--;
+            count++;
+        }
+
+        return count;
+    }
+};
