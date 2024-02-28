dfebb471f890ff94885f96164074100b2df86597
Muhammad Saqlain <77551019+MuhammadSaqlain-dev@users.noreply.github.com>
2024-02-28 22:49:32 +0500

    Time: 15 ms (14.12%) | Memory: 7.2 MB (8.20%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -3,8 +3,8 @@ public:
     int fib(int n) {
         // Minimum Possible Valid Input
         if (n==0) return 0;
-        else if (n==1) return 1;
-        
+        if (n==1) return 1;
+
         // logic
         return fib(n-1) + fib(n-2);
     }
