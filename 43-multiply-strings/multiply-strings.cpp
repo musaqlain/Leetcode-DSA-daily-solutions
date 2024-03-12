1a6b596ec591651e5010bfe75097728256e811a3
Muhammad Saqlain <77551019+MuhammadSaqlain-dev@users.noreply.github.com>
2024-03-13 00:22:17 +0500

    Time: 3 ms (78.66%) | Memory: 8.1 MB (67.48%) - LeetSync

--- 43-multiply-strings/multiply-strings.cpp
+++ 43-multiply-strings/multiply-strings.cpp
@@ -2,9 +2,21 @@ class Solution {
 public:
     string multiply(string str1, string str2) {
         if (str1 == "0" || str2 == "0") return "0";
+        string sn1 = "";
+        string sn2 = "";
 
-        const int size1 = str1.size();
-        const int size2 = str2.size();
+        if (str1[0] == '-') {
+            sn1 = "-", str1 = str1.substr(1);
+        }
+        if (str2[0] == '-') {
+            sn2 = "-", str2 = str2.substr(1);
+        }
+
+        if (sn1 == "-" && sn2 == "-") {
+            sn1=sn2="";
+        }
+
+        const int size1 = str1.size(), size2 = str2.size();
 
         string res(size1 + size2, '0');
         const int ressize = res.size();
