ba572f009faa0aa44e1fcb7c01c73ee392e07ec1
Muhammad Saqlain <77551019+MuhammadSaqlain-dev@users.noreply.github.com>
2024-03-17 23:00:19 +0500

    Time: 19 ms (34.52%) | Memory: 27 MB (47.24%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,16 +2,37 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         const int n = nums.size();
-        vector<int> res(n);
-        res[0] = 1;
-        for (int i = 1; i < n; i++) {
-            res[i] = nums[i - 1] * res[i - 1];
+
+        // create leftproduct array
+        vector<int> left(n);
+        vector<int> right(n);
+
+        // left[0] = 0
+        left[0] = 0;
+        right[n-1] = 0;
+
+        int holder = 1;
+        // 1st loop, which starts from index 1
+        for (int i=1;i<n;i++){
+            // use sliding windows approach and append the last value of that array to same indexed value of leftproduct array
+            holder *= nums[i-1];
+            left[i] = holder;
         }
-        int right = 1;
-        for (int i=n-1;i>=0;i--){
-            res[i] *= right;
-            right *= nums[i];
+
+        holder = 1;
+        // do same for the rigtproduct array
+        for (int i = n-2; i>=0; i--) {
+            holder *= nums[i+1];
+            right[i] = holder;
         }
-        return res;
+
+        // at last loop through that array and and add the rspective indexed element in a separat array and then return it
+
+        left[0] = right[0];
+        for (int i=1;i<n-1;i++){
+            left[i]*=right[i];
+        }
+
+        return left;
     }
 };
