d2de1cd07bf5e95837eee7237e0bf24fb40cfef5
Muhammad Saqlain <77551019+MuhammadSaqlain-dev@users.noreply.github.com>
2024-03-21 09:15:45 +0500

    Time: 14 ms (73.73%) | Memory: 24.3 MB (29.77%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,47 +1,20 @@
-#include <unordered_set>
-#include <vector>
-
-using namespace std;
-
 class Solution {
-public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_set<string> st;
+ public:
+  bool isValidSudoku(vector<vector<char>>& board) {
+    unordered_set<string> seen;
 
-        // Check rows
-        for (int i = 0; i < 9; i++) {
-            st.clear();
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] != '.' &&
-                    !st.insert(string(1, board[i][j])).second)
-                    return false; // Duplicate found
-            }
-        }
+    for (int i = 0; i < 9; ++i)
+      for (int j = 0; j < 9; ++j) {
+        if (board[i][j] == '.')
+          continue;
+        const string c(1, board[i][j]);
+        if (!seen.insert(c + "@row" + to_string(i)).second ||
+            !seen.insert(c + "@col" + to_string(j)).second ||
+            !seen.insert(c + "@box" + to_string(i / 3) + to_string(j / 3))
+                 .second)
+          return false;
+      }
 
-        // Check columns
-        for (int j = 0; j < 9; j++) {
-            st.clear();
-            for (int i = 0; i < 9; i++) {
-                if (board[i][j] != '.' &&
-                    !st.insert(string(1, board[i][j])).second)
-                    return false; // Duplicate found
-            }
-        }
-
-        // Check sub-grids
-        for (int i = 0; i < 9; i += 3) {
-            for (int j = 0; j < 9; j += 3) {
-                st.clear();
-                for (int x = i; x < i + 3; x++) {
-                    for (int y = j; y < j + 3; y++) {
-                        if (board[x][y] != '.' &&
-                            !st.insert(string(1, board[x][y])).second)
-                            return false; // Duplicate found
-                    }
-                }
-            }
-        }
-
-        return true; // Sudoku is valid
-    }
+    return true;
+  }
 };
