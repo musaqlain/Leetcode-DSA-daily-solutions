33e5b5e0a7b30291dcb68b997954a142b112a14e
Muhammad Saqlain <77551019+MuhammadSaqlain-dev@users.noreply.github.com>
2024-05-01 15:09:30 +0500

    Time: 241 ms (93.69%) | Memory: 133.5 MB (57.81%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.cpp
+++ 1023-time-based-key-value-store/time-based-key-value-store.cpp
@@ -1,28 +1,38 @@
 class TimeMap {
 public:
     unordered_map<string, vector<pair<int, string>>> mp;
-    TimeMap() {
-    }
-    
+    TimeMap() {}
+
     void set(string key, string value, int timestamp) {
         mp[key].push_back({timestamp, value});
     }
-    
+
     string get(string key, int timestamp) {
         if (mp.find(key) != mp.end()) {
+            // Binary Search
             vector<pair<int, string>>& valObj = mp[key];
             int l = 0, r = valObj.size() - 1;
 
             while (l <= r) {
                 int mid = l + (r - l) / 2;
 
-                if (valObj[mid].first == timestamp) return valObj[mid].second;
-                else if (valObj[mid].first < timestamp) l = mid + 1;
-                else r = mid - 1;
+                if (valObj[mid].first == timestamp)
+                    return valObj[mid].second;
+                else if (valObj[mid].first < timestamp)
+                    l = mid + 1;
+                else
+                    r = mid - 1;
             }
-            
-            if (r >= 0) return valObj[r].second;
+            if (r >= 0)
+                return valObj[r].second;
         }
         return "";
     }
 };
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
