81ab0d46de7375b0a803a2ff019f6ca76e128b0d
Muhammad Saqlain <77551019+MuhammadSaqlain-dev@users.noreply.github.com>
2024-05-18 18:19:41 +0500

    Time: 37 ms (5.78%) | Memory: 76 MB (82.14%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -1,45 +1,28 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* tmp1 = l1; ListNode* tmp2 = l2; 
-        ListNode* rNode = new ListNode(-1);
-        ListNode* rNodeCopy = rNode;
+        ListNode dummy(-1);
+        ListNode* rNodeCopy = &dummy;
         int carry = 0;
 
-        while (tmp1 != nullptr || tmp2 != nullptr) {
-            int val1 = 0, val2 = 0;
-            if (tmp1 != nullptr) {
-                val1 = tmp1->val;
-                tmp1 = tmp1->next;
-            }
+        while (l1 != nullptr || l2 != nullptr) {
+            int val1 = (l1 != nullptr) ? l1->val : 0;
+            int val2 = (l2 != nullptr) ? l2->val : 0;
 
-            if (tmp2 != nullptr) {
-                val2 = tmp2->val;
-                tmp2 = tmp2->next;
-            }
+            int sum = carry+val1+val2;
 
-            ListNode* sumNode = new ListNode((carry+val1+val2)%10);
-            carry = (carry+val1+val2)/10;
-            rNodeCopy->next = sumNode;
+            rNodeCopy->next = new ListNode(sum%10);
+            carry = sum/10;
             rNodeCopy = rNodeCopy->next;
+
+            if (l1 != nullptr) l1=l1->next;
+            if (l2 != nullptr) l2=l2->next;
         }
 
-        if (carry != 0) {
-            ListNode* sumNode = new ListNode(carry);
-            rNodeCopy->next = sumNode;
-            rNodeCopy = rNodeCopy->next;
+        if (carry> 0) {
+            rNodeCopy->next = new ListNode(carry);
         }
 
-        return rNode->next;
+        return dummy.next;
     }
 };
