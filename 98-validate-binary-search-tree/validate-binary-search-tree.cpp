67afd50f8644ae389da4c3652d3295f1115ea896
Muhammad Saqlain <77551019+MuhammadSaqlain-dev@users.noreply.github.com>
2024-05-28 04:01:50 +0500

    Time: 7 ms (75.96%) | Memory: 20.1 MB (88.36%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     bool isValidBST(TreeNode* root) {
         bool ans = true;
-        dfs(root, {(long long)INT_MIN - 1, (long long)INT_MAX + 1}, ans);
+        dfs(root, {static_cast<long long>(INT_MIN)-1,static_cast<long long>(INT_MAX)+1}, ans);
         return ans;
     }
-    void dfs(TreeNode* node, vector<long long> range, bool& ans) {
-        if (!node) return;
-        if (!ans) return;
+    void dfs(TreeNode* node, pair<long long, long long> range, bool& ans) {
+        if (!node || !ans) return;
 
-        if (node->val<=range[0] || node->val>=range[1]) {
+        if (node->val<=range.first || node->val>=range.second) {
             ans = false;
             return;
         } else {
-            dfs(node->left, {range[0], node->val}, ans);
-            dfs(node->right, {node->val, range[1]}, ans);
+            dfs(node->left, {range.first, node->val}, ans);
+            dfs(node->right, {node->val, range.second}, ans);
         }
     }
 };
