7cfddfaaf12514007eab5508037083fec0842ff2
Muhammad Saqlain <77551019+MuhammadSaqlain-dev@users.noreply.github.com>
2024-05-28 21:06:55 +0500

    Time: 10 ms (80.86%) | Memory: 23 MB (7.76%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -1,14 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<int> values;
@@ -19,8 +8,8 @@ public:
     }
     void dfs(TreeNode* node) {
         if (!node) return;
-        values.push_back(node->val);
         dfs(node->left);
+        values.push_back(node->val);
         dfs(node->right);
     }
 };
