3d6820c2312005d29c092d42cedcbd59832daca9
Muhammad Saqlain <77551019+MuhammadSaqlain-dev@users.noreply.github.com>
2024-05-28 21:07:32 +0500

    Time: 7 ms (94.42%) | Memory: 22.8 MB (19.49%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> values;
     int kthSmallest(TreeNode* root, int k) {
         dfs(root);
-        sort(values.begin(), values.end());
+        // sort(values.begin(), values.end());
         return values[k-1];
     }
     void dfs(TreeNode* node) {
