472fc9aa02e9b91b70105151f0100c2b5f6b6ad4
Muhammad Saqlain <77551019+MuhammadSaqlain-dev@users.noreply.github.com>
2024-05-31 02:10:48 +0500

    Time: 0 ms (100.00%) | Memory: 12.5 MB (51.22%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,25 +1,26 @@
 class Solution {
 private:
     vector<vector<int>> res;
-    int tar;
+
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        res.clear();
-        tar = target;
-        dfs(0, 0, {}, candidates);
+        vector<int> curr;
+        dfs(0, target, curr, candidates);
         return res;
     }
-    void dfs(int i,int total, vector<int> curr, vector<int>& candidates) {
+    void dfs(int i, int target, vector<int>& curr, vector<int>& candidates) {
         // basecases
-        if (total==tar) {res.push_back(curr); return;}
-        if (i>=candidates.size() || total>tar) return;
+        if (target == 0) {
+            res.push_back(curr);
+            return;
+        }
+        if (i >= candidates.size() || target < 0)
+            return;
 
-        total += candidates[i];
         curr.push_back(candidates[i]);
-        dfs(i, total, curr, candidates);
+        dfs(i, target - candidates[i], curr, candidates);
 
         curr.pop_back();
-        total -= candidates[i];
-        dfs(i+1, total, curr, candidates);
+        dfs(i + 1, target, curr, candidates);
     }
 };
