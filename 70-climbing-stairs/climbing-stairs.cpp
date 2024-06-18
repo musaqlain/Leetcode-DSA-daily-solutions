8b2be885327d3465b742585bd260053dfa8ce444
Muhammad Saqlain <77551019+MuhammadSaqlain-dev@users.noreply.github.com>
2024-06-18 11:52:01 +0500

    Time: 0 ms (100.00%) | Memory: 7.3 MB (32.05%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -3,13 +3,15 @@ public:
     int climbStairs(int n) {
         if (n==1) return 1;
         vector<int> dp(n+1, -1);
-        dp[0] = 1;
-        dp[1] = 1;
+        int prev2 = 1;
+        int prev = 1;
 
         for (int i=2; i<=n; i++) {
-          dp[i] = dp[i-1] + dp[i-2];
+          int curr_i = prev + prev2;
+          prev2 = prev;
+          prev = curr_i;
         }
 
-        return dp[n];
+        return prev;
     }
 };
