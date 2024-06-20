54a6be76aa36d41e972683ad1f03c04e7536672a
Muhammad Saqlain <77551019+MuhammadSaqlain-dev@users.noreply.github.com>
2024-06-20 13:21:41 +0500

    Time: 0 ms (100.00%) | Memory: 9.4 MB (10.67%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size() + 1, -1);
-        return dfs(0, nums, dp);
-    }
+        int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
 
-    int dfs(int i, vector<int>& nums, vector<int>& dp) {
-        // basecases
-        if (i>=nums.size()) return 0;
-        if (i==nums.size()-1) return nums[i];
+        vector<int> dp(n + 1, -1);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
 
-        if (dp[i]!=-1) return dp[i];
-
-        int left = nums[i] + dfs(i+2, nums, dp);
-        int right = dfs(i+1, nums, dp);
-
-        return dp[i] = max(left, right);
+        for (int i=2; i<n; i++) {
+            dp[i] = max(nums[i]+dp[i-2], dp[i-1]);
+        }
+        return dp[n-1];
     }
 };
