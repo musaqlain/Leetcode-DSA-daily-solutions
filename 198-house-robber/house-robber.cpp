aff45fb82d16d1c4de40ddbe19573e7c76e24aa5
Muhammad Saqlain <77551019+MuhammadSaqlain-dev@users.noreply.github.com>
2024-06-20 13:25:28 +0500

    Time: 7 ms (31.47%) | Memory: 9.4 MB (5.24%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -5,13 +5,15 @@ public:
         if (n == 0) return 0;
         if (n == 1) return nums[0];
 
-        vector<int> dp(n + 1, -1);
+        vector<int> dp(n, -1);
         dp[0] = nums[0];
         dp[1] = max(nums[0], nums[1]);
 
         for (int i=2; i<n; i++) {
             dp[i] = max(nums[i]+dp[i-2], dp[i-1]);
         }
+
+        for (auto it:dp) cout<<it<<endl;
         return dp[n-1];
     }
 };
