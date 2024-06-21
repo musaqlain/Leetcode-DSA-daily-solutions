adff3fcf968b0ea73a2baa7cb9bc373fcfbf2339
Muhammad Saqlain <77551019+MuhammadSaqlain-dev@users.noreply.github.com>
2024-06-21 14:43:08 +0500

    Time: 3 ms (95.57%) | Memory: 7.5 MB (73.02%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        const int n = s.size();
+        int count = 0;
+        for (int i=0; i<n; i++) {
+            // Even length substrings
+            int l=i, r=i;
+            while (l>=0 && r<=n-1 && s[l] == s[r]) {
+                count++;
+                l--, r++;
+            }
+            // Odd length substrings
+            l=i, r=i+1;
+            while (l>=0 && r<=n-1 && s[l] == s[r]) {
+                count++;
+                l--, r++;
+            }
+        }
+
+        return count;
+    }
+};
