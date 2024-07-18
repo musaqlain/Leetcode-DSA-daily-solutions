4eaf1c7bf2e044138a91ff98deaba991f88655ac
Muhammad Saqlain <77551019+MuhammadSaqlain-dev@users.noreply.github.com>
2024-07-18 19:02:27 +0500

    Time: 496 ms (22.92%) | Memory: 295 MB (8.35%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -3,15 +3,15 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         const int n = nums.size();
         vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        return dfs(0, -1, nums, dp);
+        return dfs(0, -1, nums, dp, n);
     }
-    int dfs(int i, int prev, vector<int>& nums, vector<vector<int>>& dp) {
-        if (i == nums.size()) return 0;
+    int dfs(int i, int prev, vector<int>& nums, vector<vector<int>>& dp, int n) {
+        if (i == n) return 0;
         if (dp[i][prev+1] != -1) return dp[i][prev+1];
 
-        int len = 0 + dfs(i+1, prev, nums, dp);
+        int len = 0 + dfs(i+1, prev, nums, dp, n);
         if (prev == -1 || nums[i] > nums[prev]) {
-            len = max(len, 1 + dfs(i+1, i, nums, dp));
+            len = max(len, 1 + dfs(i+1, i, nums, dp, n));
         }
 
         return dp[i][prev+1] = len;
