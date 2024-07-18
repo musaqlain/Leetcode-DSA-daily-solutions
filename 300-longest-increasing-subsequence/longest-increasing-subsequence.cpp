271ccee8767fe8da70a53739c5768f3d0b9e4ed3
Muhammad Saqlain <77551019+MuhammadSaqlain-dev@users.noreply.github.com>
2024-07-18 19:01:57 +0500

    Time: 539 ms (11.50%) | Memory: 294.9 MB (8.35%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -6,16 +6,10 @@ public:
         return dfs(0, -1, nums, dp);
     }
     int dfs(int i, int prev, vector<int>& nums, vector<vector<int>>& dp) {
-
-        // base
         if (i == nums.size()) return 0;
-
         if (dp[i][prev+1] != -1) return dp[i][prev+1];
 
-        // not take 
         int len = 0 + dfs(i+1, prev, nums, dp);
-
-        // take
         if (prev == -1 || nums[i] > nums[prev]) {
             len = max(len, 1 + dfs(i+1, i, nums, dp));
         }
