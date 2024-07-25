a324bf9d59a52e05161e53f997094e901e4827be
Muhammad Saqlain <77551019+MuhammadSaqlain-dev@users.noreply.github.com>
2024-07-25 19:15:15 +0500

    Time: 92 ms (97.66%) | Memory: 17.1 MB (65.03%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -3,34 +3,28 @@ from collections import deque
 
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        maxi = 0
+        max_area = 0
 
         m, n = len(grid), len(grid[0])
-        vis = [[0] * n for _ in range(m)]
 
-        def bfs(row, col, grid, vis):
-            vis[row][col] = 1
-
-            q = deque([(row, col)])
+        def dfs(row, col, grid):
+            # base condition
+            if (0 > row or row >= m or 0 > col or col >= n or grid[row][col] == 0):
+                return 0
+            grid[row][col] = 0
             area = 1
 
-            while q:
-                row, col = q.popleft()
-                directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-                for dr, dc in directions:
-                    nrow, ncol = row + dr, col + dc
-                    if (0 <= nrow < m and 0 <= ncol < n and vis[nrow][ncol] == 0 and grid[nrow][ncol] == 1):
-                        q.append((nrow, ncol))
-                        vis[nrow][ncol] = 1
-                        area += 1
-
+            area += dfs(row-1, col, grid)
+            area += dfs(row, col-1, grid)
+            area += dfs(row+1, col, grid)
+            area += dfs(row, col+1, grid)
 
             return area
 
         for row in range(m):
             for col in range(n):
-                if (grid[row][col] == 1 and vis[row][col] == 0):
-                    curr = bfs(row, col, grid, vis)
-                    maxi = max(maxi, curr)
+                if (grid[row][col] == 1):
+                    curr = dfs(row, col, grid)
+                    max_area = max(max_area, curr)
         
-        return maxi
+        return max_area
