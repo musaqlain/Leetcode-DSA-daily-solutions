953081bf732dcbba222e55d4b6edd3b1449c4d3b
Muhammad Saqlain <77551019+MuhammadSaqlain-dev@users.noreply.github.com>
2024-07-29 19:51:57 +0500

    Time: 41 ms (20.98%) | Memory: 16.4 MB (62.92%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,30 +1,17 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # hash vector array
-        # STEP #1
-        dp = [-1] * (len(nums) +1) # n + 1
+        n = len(nums)
+        if (n == 0):
+            return 0
+        if n == 1:
+            return nums[0]
 
-        def dfs(i, nums):
-            # base condition
-            if i >= len(nums):
-                return 0
+        prev2 = nums[0]
+        prev = max(nums[0], nums[1])
 
-            if len(nums) - 1 == i:
-                return nums[i]
+        for i in range(2, n):
+            curr = max(nums[i] + prev2, prev)
+            prev2 = prev
+            prev = curr
 
-            # STEP #2
-            if (dp[i] != -1):
-                return dp[i]
-
-            # Not Take
-            left = dfs(i+1, nums)
-
-            # Take
-            right = nums[i] + dfs(i+2, nums)
-
-            # return maximum
-            # STEP #3
-            dp[i] = max(left, right)
-            return dp[i] 
-
-        return dfs(0, nums)
+        return prev
