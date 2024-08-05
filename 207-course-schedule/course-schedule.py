273f54387e25e8224e9c79eb094f377153135b47
Muhammad Saqlain <77551019+MuhammadSaqlain-dev@users.noreply.github.com>
2024-08-05 09:45:29 +0500

    Time: 93 ms (38.37%) | Memory: 18.2 MB (71.42%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,23 +1,25 @@
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+    def canFinish(self, nums: int, preq: List[List[int]]) -> bool:
         adj_list = defaultdict(list)
-        indegree = [0] * numCourses
+        indegree = [0] * nums
+
+        for crs, p in preq:
+            adj_list[p].append(crs)
+            indegree[crs] += 1
         
-        # populate ad_list and indegree array
-        for course, prereq in prerequisites:
-            adj_list[prereq].append(course)
-            indegree[course] += 1
+        q = deque([i for i in range(nums) if indegree[i] == 0])
+
+        # Kahn's Algo
+        ans = []
+
+        while q:
+            crs = q.popleft()
+            ans.append(crs)
+
+            for n in adj_list[crs]:
+                indegree[n] -= 1
+                if indegree[n] == 0:
+                    q.append(n)
+
         
-        queue = deque([i for i in range(numCourses) if indegree[i] == 0])
-        processed_courses = 0
-        
-        while queue:
-            course = queue.popleft()
-            processed_courses += 1
-            
-            for next_course in adj_list[course]:
-                indegree[next_course] -= 1
-                if indegree[next_course] == 0:
-                    queue.append(next_course)
-        
-        return processed_courses == numCourses
+        return len(ans) == nums
