6e23b6e9e0f54cf9d3f4b6965dc9c28516b21e8b
Muhammad Saqlain <77551019+MuhammadSaqlain-dev@users.noreply.github.com>
2024-08-22 09:24:21 +0500

    Time: 40 ms (23.72%) | Memory: 16.5 MB (36.85%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,22 +1,20 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # create a 2d array
-        cache = [[0]*n for _ in range(m)]
+        # 2d
+        record = [[0] * n for _ in range(m)]
 
-        def dfs(r, c):
+        def rec(r, c):
             # base
-            if r==m-1 and c==n-1:
+            if r == m-1 and c == n-1:
                 return 1
-            
-            # out of bound condition
-            if r<0 or r>=m or c<0 or c>=n:
+            if r<0 or r>=m or c < 0 or c>= n:
                 return 0
             
-            if cache[r][c] != 0:
-                return cache[r][c]
+            if record[r][c] != 0:
+                return record[r][c]
 
-            # main
-            cache[r][c] = dfs(r+1, c) + dfs(r, c+1)
-            return cache[r][c]
+            # explore the possible ways
+            record[r][c] = rec(r+1, c) + rec(r, c+1)
+            return record[r][c]
 
-        return dfs(0, 0)
+        return rec(0, 0) # record[0][0]
