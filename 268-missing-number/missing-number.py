46801aee445ea43e8979a4372bd6a7ddd227b094
Moh Saqlain <77551019+MohSaqlainn@users.noreply.github.com>
2025-01-31 17:43:55 +0500

    Time: 20 ms (17.08%) | Memory: 19.1 MB (9.95%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,14 +1,12 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        xor1 = 0
-        xor2 = 0
-        count = 0
-        for num in nums:
-            xor1 = xor1 ^ count
-            count += 1
-            xor2 = xor2 ^ num
-        
-        xor1 = xor1 ^ len(nums)
-
-        return xor1 ^ xor2
-        
+        dic = {}
+        lent = range(len(nums)+1)
+        print(lent)
+        for i in nums:
+            if i not in dic:
+                dic[i] = 1
+        for i in lent:
+            print(i)
+            if i not in dic:
+                return i
