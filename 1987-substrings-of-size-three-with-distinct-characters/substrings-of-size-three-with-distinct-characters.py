eb1492b19a07b71c34434e4f1ed32400f08dd7e9
Moh Saqlain <77551019+musaqlain@users.noreply.github.com>
2025-06-28 22:07:35 +0500

    Time: 0 ms (100.00%) | Memory: 18 MB (9.32%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.py
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.py
@@ -1,13 +1,30 @@
 class Solution:
     def countGoodSubstrings(self, s: str) -> int:
         count = 0
-        for i in range(len(s)-2):
+
+        # iterate the s
+        for i in range(len(s) - 2):
+            # acquire the sub string
             char1 = s[i]
-            char2 = s[i+1]
-            char3 = s[i+2]
-            
+            char2 = s[i + 1]
+            char3 = s[i + 2]
+
+            # check if the substring is good
             if char1 != char2 and char2 != char3 and char3 != char1:
-                count = count + 1
+                # good substring
+                count = count + 1 
+            
+            # automatically bad -> ignore
         
         return count
+
             
+        
+
+
+# statement done
+# basic approach done
+# question
+# let's try to code
+        
+        
