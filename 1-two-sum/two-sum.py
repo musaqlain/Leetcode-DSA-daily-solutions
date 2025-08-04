153a7d5e8abdae6d3eaa5381c2250edca1361ac1
Moh Saqlain <77551019+musaqlain@users.noreply.github.com>
2025-08-04 20:27:31 +0500

    Time: 1712 ms (27.73%) | Memory: 18.6 MB (64.65%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,16 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
-        hastb = {}
-
-        # 2 pass hashtable solution
-        for i in range(n):
-            hastb[nums[i]] = i
-
-        for i in range(n):
-            desired = target - nums[i]
-            if desired in hastb and i != hastb[desired]:
-                return [hastb[desired], i]
-            
-        return []
+        for i in range(n-1):
+            for j in range(i+1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
         
